6d8052c2196f98e1b18549921adac43a093fa2c7
unknown <colonylyfe@gmail.com>
2023-05-03 13:17:57 -0400

    webgl init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import WebGL from './WebGL/index';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AppWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 100px;
+`;
+
+const Container = styled.div`
+`;
+
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      checked: false,
+    }
+
+  }
+  render(){
+    return (
+      <AppWrapper>
+        <Container>
+          <WebGL />
+        </Container>
+      </AppWrapper>
+    );
+  }
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/WebGL/Init/index.js
@@ -0,0 +1,25 @@
+import GLC from '../GLCommander';
+
+let r = 0;
+const render = () => {
+    GLC.clear(r, 0.0, 0.0, 1.0);
+    r = r + 0.001;
+    window.requestAnimationFrame(render);
+};
+
+export default (id) => {
+    const canvas = document.querySelector(`#${id}`);
+
+    if(!canvas) {
+        return;
+    }
+
+    const gl = canvas.getContext('webgl');
+
+    if(!gl) {
+        return;
+    }
+
+    GLC.init(gl);
+    window.requestAnimationFrame(render);
+}
